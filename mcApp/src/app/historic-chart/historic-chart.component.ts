cac3f8e14ced1fb4aee8c6c2b2403fa1a6d6405e
Ponsoda <ponsoda.nacho@gmail.com>
2024-02-24 16:36:30 +0100

    New line chart with historic data

---
+++ mcApp/src/app/historic-chart/historic-chart.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import Chart from 'chart.js/auto';
+
+@Component({
+  selector: 'app-historic-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './historic-chart.component.html',
+  styleUrl: './historic-chart.component.scss'
+})
+
+export class HistoricChartComponent implements OnInit {
+
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  });
+
+  public chartHistoric : any;
+  public chartName: string = "Dades històriques";
+  dataHistoricDones: number[] = [];
+  dataHistoricHomes: number[] = [];
+  years: string[];
+
+  constructor(private http: HttpClient) { 
+  }
+
+  ngOnInit(): void {
+    this.loadHistoricData();
+  }
+
+  loadHistoricData(): void {
+    // Make your API call here using this.dataToFetch
+    this.http.get<any>(`https://raw.githubusercontent.com/Ponsoda/alcoi-m-c-dades/main/associacioSantJordi/dadesHistoriques.json`, { headers: this.headers }).subscribe(
+      (data) => {
+        
+        this.years = Object.keys(data['associats'])
+
+        for (let year of this.years) {
+          this.dataHistoricDones.push(data['associats'][year]['dones'])
+          this.dataHistoricHomes.push(data['associats'][year]['homes'])
+        }
+        
+        this.createHistoricChart(this.dataHistoricDones, this.dataHistoricHomes, this.years);
+      },
+      (error) => {
+        console.log(error)
+      }
+    );
+  }
+
+  createHistoricChart(dataDones: number[], dataHomes: number[], years: string[]){
+
+    this.chartHistoric = new Chart('chartHistoric', {
+      type: 'line',
+      data: {
+        labels: years,
+         datasets: [{
+          label: 'Dones',
+          data: dataDones,
+          fill: true,
+          backgroundColor: [
+            '#ffc0cb8f',
+          ]
+        },
+        {
+          fill:true,
+          label: 'Homes',
+          data: dataHomes,
+          backgroundColor: [
+            '#0000ff87',
+          ]
+        }],
+        },
+        options: {
+          aspectRatio:2
+        }
+    });
+  }
+}
