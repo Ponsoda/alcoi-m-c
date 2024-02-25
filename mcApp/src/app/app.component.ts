f7aa7afc169339d00e0475b9751178b60fc4d78e
Ponsoda <ponsoda.nacho@gmail.com>
2024-02-25 21:14:25 +0100

    Add new components to the main component

--- mcApp/src/app/app.component.ts
+++ mcApp/src/app/app.component.ts
@@ -4,14 +4,15 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { Navbar } from './navbar/navbar.component';
-import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { AssociacioChartComponent } from './associacio-chart/associacio-chart.component';
 import { HistoricChartComponent } from './historic-chart/historic-chart.component';
-import { FilaesChartComponent } from './filaes-chart/filaes-chart.component';
+import { FilaesCristianesChartComponent } from './filaes-cristianes-chart/filaes-cristianes-chart.component';
+import { FilaesMoresChartComponent } from './filaes-mores-chart/filaes-mores-chart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PieChartComponent, HistoricChartComponent, Navbar, HttpClientModule, FilaesChartComponent],
+  imports: [CommonModule, RouterOutlet, AssociacioChartComponent, HistoricChartComponent, Navbar, HttpClientModule, FilaesCristianesChartComponent, FilaesMoresChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -19,9 +20,17 @@ export class AppComponent {
   title = "Moros i Cristians d'Alcoi";
   types: string[] = ['associats', 'junta', 'primersTrons', 'majorals', 'cuadroHonor', 'personatgesFesters']
 
-  selectedYear: number = 2022;
+  selectedYear: number = 2014;
+  years: number[];
 
   onDropdownSelectionChange(selectedYear: number): void {
     this.selectedYear = selectedYear;
   }
+
+  handleOutputChange(newValue: number[]): void {
+    this.years = newValue;
+  }
+
+  offcanvasVisible: boolean = false;
+
 }
