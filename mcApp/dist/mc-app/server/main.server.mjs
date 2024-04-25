f5c16048ea525280fabfabe721bb8878ef3434e1
Ponsoda <ponsoda.nacho@gmail.com>
2024-04-25 09:22:46 +0200

    Published new version of the app

--- mcApp/dist/mc-app/server/main.server.mjs
+++ mcApp/dist/mc-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IAWK32IL.mjs";import"./chunk-5DW7FIGC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HKRFLBD5.mjs";import"./chunk-5DW7FIGC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
